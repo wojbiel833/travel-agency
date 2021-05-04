dcf932ae833381dc7cc36fcdaae69bf5c2885346
Wojciech Bielecki <wojbiel833@gmail.com>
2021-05-04 12:56:14 +0200

    Add Hero test

---
+++ src/components/layout/Hero/Hero.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Hero from './Hero';
+
+describe('Component Hero', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<Hero titleText="Lorem ipsum" />);
+    expect(component).toBeTruthy();
+  });
+  it('should throw error withhout required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
+});
