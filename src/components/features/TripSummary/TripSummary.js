e0c1103fa420f2c2b24ca6b08a273894d442bfad
Wojciech Bielecki <wojbiel833@gmail.com>
2021-05-10 18:39:08 +0200

    Add TripSummary tests

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './TripSummary.scss';
-import {Col} from 'react-flexbox-grid';
+import { Col } from 'react-flexbox-grid';
 
-const TripSummary = ({id, image, name, cost, days, tags}) => (
-  <Col xs={12} sm={6} lg={4} className={styles.column}>
-    <Link to={`/trip/${id}`} className={styles.link}>
-      <article className={styles.component}>
-        <img src={image} alt={name} />
-        <h3 className={styles.title}>{name}</h3>
-        <div className={styles.details}>
-          <span>{days} days</span>
-          <span>from {cost}</span>
-        </div>
-        <div className={styles.tags}>
-          {tags.map(tag => (
-            <span className={styles.tag} key={tag.toString()}>{tag}</span>
-          ))}
-        </div>
-      </article>
-    </Link>
-  </Col>
-);
+const TripSummary = ({
+  id,
+  image,
+  name,
+  cost,
+  days,
+  // , tags
+}) => {
+  const [tags] = useState(['test1', 'test2', 'test3']);
+  return (
+    <Col xs={12} sm={6} lg={4} className={styles.column}>
+      <Link to={`/trip/${id}`} className={styles.link}>
+        <article className={styles.component}>
+          <img src={image} alt={name} />
+          <h3 className={styles.title}>{name}</h3>
+          <div className={styles.details}>
+            <span>{days} days</span>
+            <span>from {cost}</span>
+          </div>
+          <div className={styles.tags}>
+            {tags.map(tag => (
+              <span className={styles.tag} key={tag.toString()}>
+                {tag}
+              </span>
+            ))}
+          </div>
+        </article>
+      </Link>
+    </Col>
+  );
+};
 
 TripSummary.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  image: PropTypes.string,
-  name: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   intro: PropTypes.string,
-  cost: PropTypes.string,
-  days: PropTypes.number,
+  cost: PropTypes.string.isRequired,
+  days: PropTypes.number.isRequired,
   tags: PropTypes.array,
 };
 
---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+  it('should render correct id', () => {
+    const id = 'abc';
+    const expectedId = `/trip/${id}`;
+    const component = shallow(<TripSummary to={expectedId} />);
+
+    // const renderedId = component.find('.link').text();
+    // expect(renderedId).toEqual(expectedId);
+    expect(component.find('.link').prop('to')).toEqual(expectedId);
+  });
+  it('should render correct src and alt in <img>', () => {
+    const expectedAlt = 'Lorem ipsum';
+    const expectedSrc = 'image.jpg';
+    const component = shallow(
+      <TripSummary src={expectedSrc} alt={expectedAlt} />
+    );
+
+    const renderedImg = component.find('img').text();
+    expect(renderedImg).toEqual(expectedAlt);
+    expect(component.find('.image').prop('src')).toEqual(expectedSrc);
+  });
+  it('should throw error without required props', () => {
+    expect(() =>
+      shallow(
+        <TripSummary
+          id="Lorem ipsum"
+          image="Lorem ipsum"
+          name="Lorem ipsum"
+          cost="Lorem ipsum"
+          days={1}
+        />
+      )
+    ).toThrow();
+  });
+  it('should render without crashing', () => {
+    const component = shallow(
+      <TripSummary
+        id="Lorem ipsum"
+        image="Lorem ipsum"
+        name="Lorem ipsum"
+        cost="Lorem ipsum"
+        days={1}
+      />
+    );
+    expect(component).toBeTruthy();
+    console.log(component.debug());
+  });
+
+  it('should render correct tags array', () => {
+    const expectedArray = ['test1', 'test2', 'test3'];
+    const component = shallow(<TripSummary tags={expectedArray} />);
+
+    const renderedArray = component.find('.tags').text();
+    expect(renderedArray).toEqual(expectedArray);
+    // expect(component.find('.tags').prop('tags')).toEqual(expectedArray);
+  });
+});
