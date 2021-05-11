7a375c33a8f44219b0b01d1682dc56cbcd6217ab
Wojciech Bielecki <wojbiel833@gmail.com>
2021-05-11 10:10:31 +0200

    Fix Datepicker

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 import { registerLocale } from 'react-datepicker';
-// , setDefaultLocale
 import pl from 'date-fns/locale/pl';
 registerLocale('pl', pl);
 
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import styles from './OrderOption.scss';
 
-const OrderOptionDate = ({ id, name, type, currentValue }) => {
-  const [startDate, setStartDate] = useState(currentValue);
+const OrderOptionDate = ({ id, name, type, currentValue, setOptionValue }) => {
   return (
     <section className={styles.text}>
       <label type={type} id={id}>
         <DatePicker
           locale="pl"
-          selected={startDate}
-          onChange={date => setStartDate(date)}
+          selected={currentValue}
+          onChange={date => setOptionValue(date)}
         />
         {name}
       </label>
     </section>
   );
 };
+
 OrderOptionDate.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string,
   type: PropTypes.string,
   currentValue: PropTypes.node,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionDate;
