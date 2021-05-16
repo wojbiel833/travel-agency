9eb8f95d2d3c1cf73bdeea5635b8e9d081f39050
Wojciech Bielecki <wojbiel833@gmail.com>
2021-05-16 22:40:50 +0200

    Add Format time function

--- src/utils/FormatTime.js
+++ src/utils/FormatTime.js
@@ -1,4 +1,12 @@
-// export const formatTime = seconds => {
-// if (arguments.length != 1) return null;
-// else return 'formatted time';
-// };
+export const formatTime = secs => {
+  if (arguments.length != 1) return null;
+  if (secs != 'number') return null;
+  if (secs < 0) return null;
+  // bez tego warunku i tak przechodzi testy, dlaczego?
+
+  const seconds = (Math.floor(secs % 60) + '').padStart(2, '0');
+  const minutes = (Math.floor((secs / 60) % 60) + '').padStart(2, '0');
+  const hours = (Math.floor((secs / 3600) % 60) + '').padStart(2, '0');
+
+  return `${hours}:${minutes}:${seconds}`;
+};
